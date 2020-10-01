95943a177c5765a94f6c3ffe36216dfb95644433
Ayush Srivastava <ayushs.1992@gmail.com>
2020-10-01 22:28:47 +0530

    Added object.fromEnteries

---
+++ 2019/object-from-entries.js
@@ -0,0 +1,8 @@
+/**
+ * Object.fromEntries()
+ * convert object to array.
+ */
+const obj = {one: 1, two: 2, three: 3};
+
+console.log(Object.entries(obj));    
+// => [["one", 1], ["two", 2], ["three", 3]]
